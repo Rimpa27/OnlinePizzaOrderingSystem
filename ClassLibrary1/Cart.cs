03de2fb7e1cbe894df87a79fead9176afc92b7c3
Neelima <margana.neelima@capgemini.com>
2024-02-27 17:51:36 +0530

    ClassDiagram

---
+++ ClassLibrary1/Cart.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class Cart
+    {
+        [Key]
+        [Required(ErrorMessage = "Name is required")]
+        public int FoodItemName { get; set; }// represents the name of the food item
+
+        [Display(Name = "Items")]
+        public virtual ICollection<CartItem> Items { get; set; }//collection of items
+                                                                //CartItem represents an individual item in the cart with its name,quantity,price
+
+
+        [Display(Name = "Total Items")]
+
+        public int TotalItems//calculates the total number of items in the cart
+        {
+            get
+            {
+                int total = 0;
+                foreach (var item in Items)
+                {
+                    total += item.Quantity;//we have created Quantity Property in CartItem class
+                }
+                return total;
+            }
+        }
+
+
+        [Display(Name = "Total Price")]
+        [DataType(DataType.Currency)]
+        public decimal TotalPrice//calculates the total price of each type of food in the cart 
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Quantity * item.Price;//we have created Price Property in CartItem class
+                }
+                return totalPrice;
+            }
+        }
+
+        [Display(Name = "Total Amount")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal TotalAmount => TotalPrice;//represents the total amount,formatted as currency using[DisplayFormat]
+
+
+        [Display(Name = "Checkout")]
+        public bool CheckoutsButton
+        {
+            get
+            {
+                return Items != null && Items.Count > 0;//cart should not be empty to enable checkout
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/CartItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class CartItem
+    {
+        [Key]
+        [Required(ErrorMessage = "Item name is required")]
+        public string CartItemName { get; set; }//represents individual product name
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than zero")]
+        [Display(Name = "Quantity")]
+        public int FoodItemQuantity { get; set; }//individual type of item quantity
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
+        [DataType(DataType.Currency)]
+        public decimal FoodItemPrice { get; set; }//individual type of item's price
+
+
+        public enum VegOrNonveg //enum represents the type of the foodItem
+        {
+            [Display(Name = "Vegetarian")] Veg,
+            [Display(Name = "Non-Vegetarian")] NonVeg
+
+        }
+        public VegOrNonveg foodtype { get; set; }// to define the type of the fooditem
+
+
+
+        [ForeignKey("FoodItemName")]//primary key of the class Cart
+        public virtual Cart Cart { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
