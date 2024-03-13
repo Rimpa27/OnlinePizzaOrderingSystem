49840d7f09f337b7eb9517a421ea83c212e4c042
Rimpa27 <160703924+Rimpa27@users.noreply.github.com>
2024-03-13 18:26:40 +0530

    all updates

---
+++ OnlinePizzaOrderingSystem/FoodApp.Data/Migrations/20240313074149_cartid.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class cartid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CartId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Users");
+        }
+    }
+}
--- OnlinePizzaOrderingSystem/FoodApp.Entities/Access/Customer.cs
+++ OnlinePizzaOrderingSystem/FoodApp.Entities/Access/Customer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using FoodApp.Entities;
 
@@ -20,6 +21,9 @@ namespace FoodApp.Entities
         [Required]
         [RegularExpression("^[0-9]{10}$")]
         public long Phone { get; set; }
+        
+        public String? CartId { get; set; }
+       
 
        
        
